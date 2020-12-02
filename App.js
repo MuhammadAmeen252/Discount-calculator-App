5286a829bb32145d29b43ee23dd760432198f85f
MuhammadAmeen252 <sheikh,ameen252@gmail.com>
2020-12-02 23:36:01 +0500

    Added modal to project

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet,View,Text, TextInput, TouchableOpacity, ScrollView, Button } from 'react-native';
+import { StyleSheet,View,Text, TextInput, TouchableOpacity, FlatList, Modal,Image } from 'react-native';
 
 
 const App=()=>{
 
   const [price,setPrice]=useState('')
   const [percentage,setPercentage]=useState('')
-  const [list,setList]=useState(['asasa','dsad'])
+  const [list,setList]=useState([])
+  const [key,setKey]=useState(0)
+  const [modalVisibility,setModalVisibility]=useState(false)
   const savedPrice=()=>{
     if(price!=='' && percentage!==''){
       if((price >0 && percentage>0 && percentage<=100)){
@@ -17,8 +19,6 @@ const App=()=>{
        and percentage must be between 0 and 100 and both must be numbers.</Text>
       }
     }
-    
-  
   }
   
   const finalPrice=()=>{
@@ -36,16 +36,48 @@ const App=()=>{
   }
   
   const handleSave=()=>{
-    console.log(price);
-    setList([...list,{price:'Actual Price: '+price,
+    setKey(key+2)
+    const item= {key:key,price:'Actual Price: Rs.'+price,
     percentage:'Discount Percentage: '+percentage,
-    priceAfterDisc:'Price after Discount: '+ (price-(Math.floor(price*(percentage/100)))).toFixed(2)}])
+    priceAfterDisc:'Price after Discount: Rs.'+ (price-(Math.floor(price*(percentage/100)))).toFixed(2)}
+    setList([...list,item])
   }
-  const handleHistory=()=>{
-
+  const showModal=()=>{
+    setModalVisibility(true)
+  }
+  const hideModal=()=>{
+    setModalVisibility(false)
   }
   return(
     <View style={styles.container}>
+      
+      <Modal visible={modalVisibility} animationType='slide'>
+        <View>
+          <View style={styles.row}>
+            <TouchableOpacity activeOpacity = { .5 } onPress={ hideModal }>
+            <Image
+            source={require('./assets/back-arrow.png')}
+            style={{ width: 40, height: 40 }}
+            />
+            </TouchableOpacity>
+            
+            <Text style={styles.modaltitleText}>History</Text>
+          </View>
+        
+          <FlatList
+          keyExtractor={(item) => item.key.toString()}
+          data={list}
+          renderItem = {({item})=>(
+            <View style={styles.modalTextContainer}>
+              <Text style={styles.displayResultText}>{item.price}{"\n"}{item.percentage}%{"\n"}
+              {item.priceAfterDisc}</Text>
+            </View>
+          
+          )}>
+        </FlatList>
+        </View>
+      </Modal>
+
       <View style={styles.title}><Text style={styles.titleText} >Discount Calculator</Text></View>
       <View style={styles.inpContainer}>
       <TextInput style={styles.textInpBox}
@@ -67,18 +99,13 @@ const App=()=>{
              <Text style={styles.buttonText}>Save</Text>
            </View>
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleHistory}>
+      <TouchableOpacity onPress={showModal}>
            <View style={styles.button}>
              <Text style={styles.buttonText}>View history</Text>
            </View>
       </TouchableOpacity>
-      <View>
-        {list.map((item)=>{
-          
-            <Text style={{color:'black'}}>{item}</Text>
-           
-        })}
-      </View>
+      
+      
     </View>
   )
 }
@@ -88,8 +115,21 @@ const styles=StyleSheet.create({
     flex: 1,
     padding: 24,
   },
+  row:{
+    flexDirection:'row'
+  }
+  ,
+  listFlat:{
+    backgroundColor:'grey'
+  },
   inpContainer:{
     margin:10,
+  },
+  modaltitleText:{
+    fontSize:30,
+    fontWeight:'bold',
+    color:'#f01d71',
+    marginLeft:'24%'
   },
   textInpBox:{
     borderColor:'#2a9df4',
@@ -99,6 +139,9 @@ const styles=StyleSheet.create({
     padding:5,  
     fontSize:18,
     color:'grey'
+  },
+  ScrollView:{
+    backgroundColor:'grey'
   },
   title:{
     width:'100%',
@@ -116,6 +159,13 @@ const styles=StyleSheet.create({
     borderRadius:10,
     borderColor:'grey',
     padding:15
+  },
+  modalTextContainer:{
+    margin:10,
+    backgroundColor:'#2a9df4',
+    borderRadius:10,
+    borderColor:'grey',
+    padding:10
   },
   displayResultText:{
     fontSize:20,
